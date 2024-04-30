5e23d169188362b3bb7692525d9e1eda5aebf4e8
Zelenskiy <Zelensk1973@gmail.com>
2024-04-30 16:38:59 +0300

    feat: add navbar #2

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import React  from 'react';
 import {  BrowserRouter, Route, Routes } from 'react-router-dom';
 import About from './pages/About';
 import Posts from './pages/Posts';
+import Error from './pages/Error';
 import Navbar from './components/UI/navbar/navbar';
 
 
@@ -13,6 +14,7 @@ function App() {
      <Routes>
       <Route path="/about" element={<About />} />
       <Route path="/posts" element={<Posts />} />
+      <Route path="/*" element={<Error />} />
     </Routes>
     </BrowserRouter>
   )
